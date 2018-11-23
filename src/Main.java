d5cb35974a05ab1f316cbcccb82ceb9a5705efca
Евгений <Main1909@ex.ua>
2018-11-23 23:46:52 +0200

    on alphabet

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,26 @@ import java.util.Comparator;
 
 public class Main {
     public static void main(String[] args) {
-        String[] strings = {"Alex", "Joe", "Steven"};
+        String[] strings = {"Joe", "Steven", "Alex", "Yaroslav", "Alexander", "Loi", "Loiment"};
         Arrays.sort(strings, new Comparator<String>() {
             @Override
             public int compare(String o1, String o2) {
-                return o1.length() - o2.length();
+                if (o1.charAt(0) == o2.charAt(0)) {
+                    int i = 0;
+                    int k = 0;
+                    while (o1.charAt(i) == o2.charAt(i)) {
+                        if (o1.charAt(i) == 0) {
+                            k = -1;
+                        } else if (o2.charAt(i) == 0) {
+                            k = 1;
+                        }
+                        i++;
+                        return k;
+                    }
+
+                    return o1.charAt(i) - o2.charAt(i);
+                }
+                return o1.charAt(0) - o2.charAt(0);
             }
         });
         System.out.println(Arrays.toString(strings));
